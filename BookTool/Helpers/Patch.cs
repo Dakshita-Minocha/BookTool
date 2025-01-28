cac8647297f319bd4302e8652b59bfb8c7cefdf6
Dakshita <dakshitaminocha@metamation.com>
2025-01-28 18:27:01 +0530

    gitk interface

--- BookTool/Helpers/Patch.cs
+++ BookTool/Helpers/Patch.cs
@@ -1,45 +1,34 @@
-﻿using System.Collections.ObjectModel;
+﻿using System.Diagnostics;
 using System.IO;
-using System.Management.Automation;
+using System.Reflection;
+using System.Text;
 using static BookTool.Error;
 namespace BookTool;
 
+#region Class Patch -------------------------------------------------------------------------------
 public static class Patch {
+   #region Properties -----------------------------------------------
    public static Repository? Source { get; set; }
    public static Repository? Target { get; set; }
-   public static string? CommitID {  get; set; }
+   public static string? CommitID { get; set; }
    public static List<string> Errors { get; } = [];
+   public static List<string> PatchFile => sPatch ?? [];
+   #endregion
 
+   #region Methods --------------------------------------------------
    public static Error Generate () {
       if (Source == null) return SetSource;
-      if (Target == null) return SetTargetRepository;
-      Collection<PSObject> results;
-      string branchName = $"changesUpto.{CommitID}";
-      using (PowerShell powershell = PowerShell.Create ()) {
-         var root = (Path.GetPathRoot (Source.Path) ?? "C:").Replace ("\\", "");
-         powershell.AddScript ($"{root}");
-         powershell.Invoke ();
-         powershell.AddScript ($"cd {root}\\;cd {Source.Path}; git sw {Source.Main}");
-         powershell.Invoke ();
-         powershell.AddScript ("git lg");
-         results = powershell.Invoke (); powershell.Streams.Error.Clear ();
-         if (results.Count is 0) return NoCommitsFound;
-         if (results.FirstOrDefault (a => a.ToString ().StartsWith ($"* {CommitID}") && a.ToString ().Contains ($"(HEAD -> master, {branchName})")) is not null)
-            return NoChangesMadeAfterCommit; // (HEAD -> master, changesUpto.f7a2d7d)
-         if (File.Exists ($"{Target.Path}/change1.DE.patch")) File.Delete ($"{Target.Path}/change1.DE.patch");
-         powershell.AddScript ($"git diff {CommitID} > change1.patch");
-         results = powershell.Invoke ();
-         powershell.AddScript ($"copy change1.patch \"{Target.Path}/change1.DE.patch\"");
-         powershell.Invoke ();
-         powershell.AddScript ($"git checkout -b {branchName} {CommitID}");
-         results = powershell.Invoke ();
-         if (results.Count is not 0)
-            powershell.AddScript ($"git branch -D {branchName}; git checkout -b {branchName} {CommitID}");
-         powershell.AddScript ($"git checkout {branchName}");
-         powershell.Invoke ();
+      RunHiddenCommandLineApp ("git.exe", $"switch {Source.Main}", out _, workingdir: Source.Path);
+      var res = RunHiddenCommandLineApp ("git.exe", $"diff {CommitID}", out int nExit, workingdir: Source.Path);
+      if (nExit != 0) {
+         Errors.AddRange (res);
+         return ErrorGeneratingPatch;
       }
+      sPatch = res;
+      if (res.Count == 0) return NoChangesMadeAfterCommit;
       return OK;
    }
+   static List<string>? sPatch;
 
    /// <summary>Used to gather data from patch and apply to laguage repositories.</summary>
    /// https://git-scm.com/docs/diff-generate-patch
@@ -56,95 +45,149 @@ public static class Patch {
    public static Error ProcessPatch () {
       if (Source == null) return SetSource;
       if (Target == null) return SetTargetRepository;
-      List<string> patchFile = [.. File.ReadAllLines ($"{Target.Path}/change1.DE.patch")];
+      if (sPatch == null) return ErrorGeneratingPatch;
       List<string> file1Content = [];
       ReadOnlySpan<char> file1 = null, file2 = null;
       int oldStartLine = -1, oldTotalLines = -1, aIndex, contextLine = 3;
-      for (int i = 0; i < patchFile.Count; i++) {
-         string line = patchFile[i];
+      bool newFile = false, overwritten = false;
+      for (int i = 0; i < sPatch.Count; i++) {
+         string line = sPatch[i];
          if ((aIndex = line.IndexOf ("diff --git a/")) != -1) {
             (oldStartLine, oldTotalLines) = (-1, -1);
             var bIndex = line.IndexOf ("b/");
-            file1 = line.AsSpan (aIndex + 13, bIndex - (aIndex + 13));
-            file1Content = [.. File.ReadAllLines ($"{Target.Path}/{file1}")];
-            file2 = line.AsSpan (bIndex + 2);
-            if (file1 != file2) { } // rename, copy
-         } else if (line.StartsWith ("@@")) { // minimum 2 '@'
-            int j = 1;
-            while (!char.IsDigit (line[j++])) ;
-            int idx = j - 1;
-            while (line[j++] != ',') ;
-            int.TryParse (line.AsSpan (idx, j - 1 - idx), out oldStartLine);
-            idx = j;
-            while (char.IsDigit (line[j++])) ;
-            int.TryParse (line.AsSpan (idx, j - 1 - idx), out oldTotalLines);
+            file1 = line.AsSpan (aIndex + 13, bIndex - (aIndex + 13)).Trim ();
+            file2 = line.AsSpan (bIndex + 2).Trim ();
+            newFile = sPatch[++i].StartsWith("new file mode"); // It may be rename/copy, file will not exist in target rep in this case.
+            if (!newFile && string.Compare (file1.ToString (), file2.ToString ()) == 0)
+               file1Content = [.. File.ReadAllLines ($"{Target.Path}/{file1}")];
+         } else if (line.StartsWith ("@@")) {
+            // @@ -2,9 +2,8 @@ = @@ -a,b + c,d @@
+            // we read 4 values and compare total lines changed. If it is a new file we consider c, d as startline and total lines
+            // else if it is in index mode, we consider whichever has lesser number of lines changed.
+            int a, b, c, d;
+            // reading a,b:
+            int j = line.IndexOf ('-') + 1, k = j + line[j..].IndexOf (','), l = k + line.AsSpan (k).IndexOf ('+');
+            int.TryParse (line.AsSpan (j, k - j), out a);
+            if (l <= k) int.TryParse (line.AsSpan (k), out b); // "@@ -1 +1,3 @@" old file was overwritten.
+            else int.TryParse (line[(k + 1)..l], out b);
+            // reading c, d
+            j = line.IndexOf ('+') + 1; k = j + line[j..].IndexOf (','); l = k + line.AsSpan (k).IndexOf ('@');
+            // @@ -0,0 +1 @@ : may or may not occur in new file mode, in which case we read the num of lines added and skip those.
+            if (k <= j) {
+               int.TryParse (line.AsSpan (j), out d); // it is a new file
+               i += d + 1; // +1 for the "/ No newline at end of file" added in patch by git.
+               continue;
+            } else int.TryParse (line[(k + 1)..l], out d);
+            int.TryParse (line.AsSpan (j, k - j), out c);
+            (oldStartLine, oldTotalLines) = (c, d);
             contextLine = i;
+            overwritten = b < d;
          } else if (oldStartLine != -1 && oldTotalLines != -1 && file1 != null && file2 != null) {
             int k, newStartLine = oldStartLine, newTotalLines = oldTotalLines;
-            // insert para lines (before generated context)
-            //for (k = oldStartLine - 1; k >= 0 && file1Content[k].EndsWith ('\n'); k--) {
-            //   patchFile.Insert (contextLine, ' ' + file1Content[k]);
-            //   newStartLine = k + 1;
-            //   newTotalLines++;
-            //}
             k = oldStartLine - 1;
-            if (k > 1) patchFile[i - 1] = patchFile[i - 1][..(patchFile[i - 1].LastIndexOf ('@') + 1)] + ' ' + file1Content[k - 2];
+            if (k > 1) sPatch[i - 1] = sPatch[i - 1][..(sPatch[i - 1].LastIndexOf ('@') + 1)] + ' ' + file1Content[k - 1]; // doesn't matter at all??????
             // replace context lines with translated text
-            for (k = 0; k < oldTotalLines; k++) {
-               switch (patchFile[i][0]) {
-                  case '+': i++; k--; break;
-                  case '-': patchFile[i++] = '-' + file1Content[oldStartLine - 1 + k]; break;
-                  case ' ': patchFile[i++] = ' ' + file1Content[oldStartLine - 1 + k]; break;
+            for (k = 0; k < oldTotalLines;) {
+               switch (sPatch[i][0]) {
+                  case '+': i++; /*if (overwritten) k++;*/ continue;
+                  case '-': sPatch[i++] = '-' + file1Content[oldStartLine - 1 + k++]; continue;
+                  case ' ': sPatch[i++] = ' ' + file1Content[oldStartLine - 1 + k++]; continue;
+                  case '\\': i++; continue; // "/ No newline at end of file"
                }
+               break;
             }
-            // insert lines to complete para (after context lines)
-            //for (k = oldStartLine + oldTotalLines; k < file1Content.Count && file1Content[k].EndsWith ('\n'); k++) patchFile.Add (file1Content[k]);
-
-            // change line value in contextLine
-            //patchFile[contextLine] = patchFile[contextLine].Replace ($"{oldStartLine},{oldTotalLines}", $"{newStartLine},{newTotalLines}");
             (oldStartLine, oldTotalLines) = (-1, -1); i--;
          }
       }
-      File.WriteAllLines ($"{Target.Path}/change1.DE.patch", patchFile);
+      //File.WriteAllLines ($"{Target.Path}/change1.DE.patch", sPatch);
       return OK;
    }
 
    public static Error Apply () {
       if (Target == null) return SetTargetRepository;
-      if (!File.Exists ($"{Target.Path}/change1.DE.patch")) return CannotApplyPatch;
-      using (PowerShell powershell = PowerShell.Create ()) {
-         var root = (Path.GetPathRoot (Target.Path) ?? "C:").Replace ("\\", "");
-         powershell.AddScript ($"{root}");
-         powershell.AddScript ($"cd {root}\\;cd \"{Target.Path}\"; git sw main");
-         powershell.Invoke ();
-         powershell.Streams.Error.Clear ();
-         powershell.AddScript ($"git apply change1.DE.patch");
-         powershell.Invoke ();
-         if (powershell.HadErrors && powershell.Streams.Error.Count > 1) { // Error [0] is 'already on main'
-            Errors.AddRange (powershell.Streams.Error.Select (x => x.Exception.Message.ToString ()));
-            return CannotApplyPatch;
-         }
-         powershell.AddScript ($"git di");
-         powershell.Invoke ();
+      if (sPatch is null) return CannotApplyPatch;
+      var results = RunHiddenCommandLineApp ("git.exe", $"switch {Target.Main}", out int nExit, workingdir: Target.Path);
+      //results = RunHiddenCommandLineApp ("git.exe", $"restore .", out nExit, workingdir: Target.Path);
+      //results = RunHiddenCommandLineApp ("git.exe", $"clean -f", out nExit, workingdir: Target.Path);
+      File.WriteAllLines ($"{Target.Path}/change1.DE.patch", sPatch);
+      results = RunHiddenCommandLineApp ("git.exe", $"apply change1.DE.patch", out nExit, workingdir: Target.Path);
+      if (nExit != 0 || results.Count != 0) {
+         Errors.AddRange (results);
+         return CannotApplyPatch;
       }
+      RunHiddenCommandLineApp ("git.exe", $"difftool --dir-diff", out _, workingdir: Target.Path);
       return OK;
    }
+   #endregion
+
+   #region Implmentation --------------------------------------------
+   /// <summary>Executes a command-line program and returns the output it produces as a list of strings</summary>
+   /// This routine captures stdout and runs a command line appliaction. Any data that application
+   /// writes to stdout will be gathered and returend as a list of strings. The app is never shown
+   /// and runs in a hidden window; if the app ever stops for any input the calling process will
+   /// wait forever. Be careful to use suitable command line arguments to prevent any prompting. 
+   /// For example, when running SVN.EXE, use the command line argument --non-interactive. 
+   /// <param name="exe">The name of the application to execute</param>
+   /// <param name="args">The command line arguments for the application</param>
+   /// <param name="nExit">This gets set to the exit code from the application you are running</param>
+   /// <param name="encoding">The text encoding to use when reading from standard output</param>
+   /// <param name="workingdir">The working directory to use (if null, uses the Current directory)</param>
+   /// <returns>A list of strings constituting the output generated by that application</returns>
+   public static List<string> RunHiddenCommandLineApp (string exe, string args, out int nExit, Encoding? encoding = null, string? workingdir = null) {
+      List<string?> output = [];
+      ProcessStartInfo startinfo = new () {
+         CreateNoWindow = true, UseShellExecute = false, Arguments = args,
+         WorkingDirectory = workingdir ?? Directory.GetCurrentDirectory (), FileName = exe,
+         RedirectStandardError = true, RedirectStandardOutput = true,
+         StandardOutputEncoding = encoding
+      };
+      if (Process.Start (startinfo) is not Process proc) throw new IOException ($"Unable to execute '{exe}'");
+      // The following is the standard protocol for reading output from a command-line app. 
+      // Make the call to BeginOutputReadLine(), and then do ReadToEnd() and THEN WaitForExit().
+      // This doesn't look very kosher, but this is the only way it works correctly. 
+      proc.OutputDataReceived += (_, e) => output.AddNonNull (e.Data);
+      proc.ErrorDataReceived += (_, e) => output.AddNonNull (e.Data);
+      proc.BeginOutputReadLine (); proc.BeginErrorReadLine ();
+      proc.WaitForExit (); nExit = proc.ExitCode;
+      proc.Close ();
+      return [.. output];
+   }
+
+   /// <summary>Add an element if it is non-null</summary>
+   static void AddNonNull<T> (this IList<T> list, T value) {
+      if (value != null) list.Add (value);
+   }
+
+   /// <summary>Returns an absolute filename, given a filename relative to Flux.dll.</summary>
+   /// This does not check if the file or path you specify here exists. It just computes the
+   /// filename and returns it. 
+   static string GetLocalFile (string file) {
+      string? location = Assembly.GetEntryAssembly ()?.Location ?? GetLocation (Assembly.GetExecutingAssembly () ?? Assembly.GetCallingAssembly ());
+      return Path.GetFullPath (Path.Combine (Path.GetDirectoryName (location) ?? "", file));
+   }
+
+   // Helpers ..............................
+   static string? GetLocation (Assembly asm) {
+      if (asm == null) return null;
+      return new Uri (asm.Location).LocalPath;
+   }
+   #endregion
 }
+#endregion
 
+#region Record Repository -------------------------------------------------------------------------
 public record Repository (string Path, string Main);
+#endregion
 
-#region Enum Error ---------------------------------------------------
+#region Enum Error --------------------------------------------------------------------------------
 public enum Error {
    OK = 0,
    SetSource,
    SetTargetRepository,
    SelectedFolderIsNotARepository,
-   NoCommitsFound,
-   InvalidCommitID,
    NoChangesMadeAfterCommit,
    ErrorGeneratingPatch,
    CannotApplyPatch,
-   PatchApplied,
    Clear
 }
 #endregion
--- BookTool/MainWindow.xaml.cs
+++ BookTool/MainWindow.xaml.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.Win32;
 using System.Globalization;
 using System.IO;
-using System.Management.Automation;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
@@ -17,11 +16,10 @@ public partial class MainWindow : Window {
       InitializeComponent ();
       mGTLCommand = new RelayCommand (GoToLine, ContentLoaded);
       InputBindings.Add (new KeyBinding (mGTLCommand, mGTLGesture));
-      mFileTree.ItemsSource = mTreeViewItems;
       mRunHeight = (int)new FormattedText ("9999", CultureInfo.CurrentCulture, FlowDirection.LeftToRight,
-                        new Typeface (mFlowDoc.FontFamily.ToString ()), mFlowDoc.FontSize, System.Windows.Media.Brushes.Black, null, 1).Height;
+                        new Typeface (mENDoc.FontFamily.ToString ()), mENDoc.FontSize, Brushes.Black, null, 1).Height;
       Loaded += delegate {
-         mScroller = (ScrollViewer)((Decorator)VisualTreeHelper.GetChild (VisualTreeHelper.GetChild (mDocScroller, 0), 0)).Child;
+         mScroller = (ScrollViewer)((Decorator)VisualTreeHelper.GetChild (VisualTreeHelper.GetChild (mENDocScroll, 0), 0)).Child;
          //mBtnExport.InputBindings.Add (new CommandBinding (mContentLoaded, (s, e) => mBtnExport.IsEnabled = !IsEnabled));
       };
    }
@@ -40,28 +38,22 @@ public partial class MainWindow : Window {
    ScrollViewer? mScroller;
    readonly int mRunHeight;
 
-   Error ValidateCommitID () {
-      if (mTBCommitID.Text is not string id || id.Length < 6 || id.Any (x => !char.IsAsciiLetterOrDigit (x) || char.IsWhiteSpace (x))) return InvalidCommitID;
-      CommitID = id;
-      return OK;
-   }
+   void Reset () { UpdateDoc (mLangDoc, Clear); UpdateDoc (mENDoc, Clear); mTreeViewItems.Clear (); mBtnApply.IsEnabled = false; mChanges.Clear (); }
 
-   void Reset () { UpdateDoc (Clear); mFileTree.ItemsSource = null; mTreeViewItems.Clear (); mBtnApply.IsEnabled = false; mChanges.Clear (); }
-
-   void UpdateDoc (Error err) {
-      mFlowDoc.Blocks.Clear ();
+   void UpdateDoc (FlowDocument doc, Error err) {
+      doc.Blocks.Clear ();
       var para = new Paragraph ();
       switch (err) {
          case OK:
             mChanges.ForEach (line => para.Inlines.Add (new Run ($"{line}\n") {
-               Background = line[0] is '+' ? System.Windows.Media.Brushes.GreenYellow :
-                            line[0] is '-' ? System.Windows.Media.Brushes.Red :
-                            System.Windows.Media.Brushes.Transparent
+               Background = line[0] is '+' ? Brushes.GreenYellow :
+                            line[0] is '-' ? Brushes.Red :
+                            Brushes.Transparent
             })); mContentLoaded = true; mMaxRows = mChanges.Count + 1; break;
          case Clear: mContentLoaded = false; break;
          default: para.Inlines.Add (new Run ($"Error: {err}\n")); Errors.ForEach (x => para.Inlines.Add (new Run ($"{x}\n"))); Errors.Clear (); mContentLoaded = true; break;
       }
-      mFlowDoc.Blocks.Add (para);
+      doc.Blocks.Add (para);
    }
    #endregion
 
@@ -70,9 +62,15 @@ public partial class MainWindow : Window {
       => mTreeScroll.ScrollToVerticalOffset (-e.Delta + mTreeScroll.VerticalOffset);
 
    void OnSelectionChanged (object sender, RoutedPropertyChangedEventArgs<object> e) {
-      //if (sender is not TreeView tv || tv.SelectedItem is not TreeViewItem selected) return;
-      //selected.IsExpanded = !selected.IsExpanded;
-      //if (selected.Tag is not FileInfo file) return;
+      if (sender is not TreeView tv || tv.SelectedItem is not string selected) return;
+      CommitID = selected.Split ("  ")[0];
+      Reset ();
+      Error err;
+      if ((err = Generate ()) != OK) { UpdateDoc (mENDoc, err); return; }
+      err = ProcessPatch ();
+      if (err == OK) mChanges = PatchFile;
+      UpdateDoc (mENDoc, err);
+      mBtnApply.IsEnabled = true;
    }
 
    /// <summary>Navigates to user entered line number.</summary>
@@ -87,52 +85,86 @@ public partial class MainWindow : Window {
       if (sender is not Button btn) return;
       Reset ();
       OpenFolderDialog fd = new () { Multiselect = false, DefaultDirectory = "C:" };
-      if (fd.ShowDialog () == true) UpdateDoc (SetRep (btn.Name == "mOpenEN"));
+      if (fd.ShowDialog () == true) UpdateDoc (mENDoc, SetRep (btn.Name == "mOpenEN"));
 
       // Helper Methods ---------------------------------------------
       Error SetRep (bool source) {
          string folderPath = fd.FolderName;
          if (Directory.GetDirectories (folderPath, ".git", SearchOption.TopDirectoryOnly).Length == 0) return SelectedFolderIsNotARepository;
-         using (PowerShell powershell = PowerShell.Create ()) {
-            powershell.AddScript ($"cd {(Path.GetPathRoot (folderPath) ?? "C:\\").Replace ("\\", "")}");
-            var results = powershell.Invoke ();
-            powershell.AddScript ($"cd \"{folderPath}\"");
-            powershell.AddScript ("git branch");
-            results = powershell.Invoke ();
-            Repository rep = new (folderPath, results.Select (a => a.ToString ()).First (a => a.Contains ("main") || a.Contains ("master")).Trim ('*'));
-            if (source) {
-               Source = rep;
-               mSelectedRep.Content = folderPath;
-            } else {
-               Target = rep;
-               mTargetRep.Content = folderPath;
-            }
+         var results = RunHiddenCommandLineApp ("git.exe", $"branch", out _, workingdir: folderPath);
+         Repository rep = new (folderPath, results.Select (a => a.ToString ()).First (a => a.Contains ("main") || a.Contains ("master")).Trim ('*'));
+         if (source) {
+            Source = rep;
+            mSelectedRep.Content = folderPath;
+            RunHiddenCommandLineApp ("git.exe", $"switch {Source.Main}", out int nExit, workingdir: folderPath);
+            results = RunHiddenCommandLineApp ("git.exe", "log -n 30 --format=\"%h  %s\"", out nExit, workingdir: folderPath);
+            mCommitTree.ItemsSource = results.ToList ();
+         } else {
+            Target = rep;
+            mTargetRep.Content = folderPath;
          }
          return OK;
       }
-   }
-
-   void OnClickContentLoad (object sender, RoutedEventArgs e) {
-      UpdateDoc (Clear);
-      Error err;
-      if ((err = ValidateCommitID ()) != OK) { UpdateDoc (err); return; }
-      if ((err = Generate ()) != OK) { UpdateDoc (err); return; }
-      err = ProcessPatch (); mChanges = [.. File.ReadAllLines ($"{Target?.Path}/Change1.DE.patch")]; UpdateDoc (err);
-      //mFileTree.ItemsSource = mTreeViewItems.Select (a => new TreeViewItem () { Header = a.Name, Tag = a });
-      mBtnApply.IsEnabled = true;
    }
    static List<FileInfo> mTreeViewItems = [];
    static List<string> mChanges = [];
 
    void OnClickApply (object sender, RoutedEventArgs e) {
-      Reset ();
-      if (Apply () == OK) mChanges.Add ("Patch Applied.");
-      UpdateDoc (OK);
+      var err = Apply ();
+      UpdateDoc (mLangDoc, err);
+      if (err == OK) PopulateTreeView ();
+      else mFileTree.ItemsSource = null;
    }
 
-   void OnTBMouseDown (object sender, MouseButtonEventArgs e) {
-      if (sender is not TextBox tb) return;
-      if (tb.Text == "Last Translated Commit ID") tb.Text = "";
+   /// <summary>Populates Treeview with underlying directories and files.</summary>
+   void PopulateTreeView () {
+      mFileTree.Items.Clear ();
+      var info = new DirectoryInfo (Target!.Path);
+      var dir = info.GetDirectories ().Where (a => !a.Name.StartsWith ('.'));
+      foreach (var folder in dir) {
+         var item = MakeTVI (folder.Name);
+         if (Path.GetDirectoryName (folder.Name) is string path) AddSubItems (path, item);
+         if (item.HasItems) mFileTree.Items.Add (item);
+      }
+      var item2 = MakeTVI (info.Name, expanded: true);
+      AddSubItems (Target.Path, item2);
+      if (item2.HasItems) mFileTree.Items.Add (item2);
+   }
+
+   /// <summary>Adds Sub-Directories and Files from a given file path as per specified pattern</summary>
+   /// <param name="pattern">SearchPattern for file, if any.</param>
+   void AddSubItems (string path, TreeViewItem item, string pattern = "", bool expanded = false) {
+      var dirInfo = new DirectoryInfo (path);
+      var dirs = dirInfo.GetDirectories ().Where (a => !a.Name.StartsWith ('.'));
+      foreach (var directory in dirs) {
+         var tvi = MakeTVI (directory.Name, directory.FullName, expanded);
+         AddSubItems (directory.FullName, tvi, pattern);
+         if (tvi.HasItems) item.Items.Add (tvi);
+      }
+      var files = dirInfo.GetFiles (pattern);
+      foreach (var file in files)
+         item.Items.Add (MakeTVI (file.Name, file.FullName));
+   }
+
+   // Helper method to return TreeViewItem with given parameters with OnSelected, OnTreeViewExpanded Handlers attached.
+   TreeViewItem MakeTVI (string header, string? tag = null, bool expanded = false) {
+      var tvi = new TreeViewItem () { Header = header, Tag = tag, IsExpanded = expanded };
+      tvi.Selected += OnSelected;
+      return tvi;
+   }
+
+   void OnSelected (object sender, RoutedEventArgs e) {
+      if (sender is not TreeViewItem tv) return;
+      if (tv.HasItems) tv.IsExpanded = !tv.IsExpanded;
+      else if (tv.Tag is string path) {
+         var file = File.ReadAllLines (path).ToList ();
+         mLangDoc.Blocks.Clear ();
+         var para = new Paragraph ();
+         int count = 1;
+         file.ToList ().ForEach ((line) => { para.Inlines.Add (new Run ($"{count++,4}│") { Foreground = Brushes.Red }); para.Inlines.Add (new Run ($"{line}\n")); });
+         mLangDoc.Blocks.Add (para);
+         mContentLoaded = true;
+      }
    }
    #endregion
 }
